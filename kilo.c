3f741d51dbecd7439c2afdc18987f48768cf28ec
Charles Wood <chwoodv@gmail.com>
2025-05-29 23:39:07 +0100

    Text is displayed from input file

--- kilo.c
+++ kilo.c
@@ -1,5 +1,9 @@
 /*** includes ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -7,9 +11,10 @@
 #include <termios.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <string.h>
 
-/*** defines ***/
+/*** ------------------------- defines ------------------------- ***/
 
 #define KILO_VERSION "0.0.1"
 
@@ -27,18 +32,25 @@ enum editorKey {
     PAGE_DOWN
 };
 
-/*** data ***/
+/*** ------------------------- data ------------------------- ***/
+
+typedef struct erow {
+    int size;
+    char *chars;
+} erow;
 
 struct editorConfig {
     int cx, cy;
     struct termios orig_termios;
+    int numrows;
+    erow *row;
     int screenrows;
     int screencols;
 };
 
 struct editorConfig E;
 
-/*** terminal ***/
+/*** ------------------------- terminal ------------------------- ***/
 
 /**
  * call to exit program (CTRL Q or error)
@@ -166,7 +178,43 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
-/*** append buffer ***/
+/*** ------------------------- row operations ------------------------- ***/
+
+void editorAppendRow(char *s, size_t len) {
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+
+/*** ------------------------- file i/o ------------------------- ***/
+
+/**
+ * Open and read file from disk
+ */
+void editorOpen(char *filename){
+    FILE *fp = fopen(filename, "r");        // VSCode creates an error here saying FILE is not in stdio for some reason, ignore this
+    if (!fp) die("fopen");
+    
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
+        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+            linelen--;
+
+        editorAppendRow(line, linelen);
+    }
+    free(line);
+    fclose(fp);
+}
+
+/*** ------------------------- append buffer ------------------------- ***/
 
 struct abuf {
     char *b;
@@ -187,7 +235,7 @@ void abFree(struct abuf *ab) {
     free(ab->b);
 }
 
-/*** output ***/
+/*** ------------------------- output ------------------------- ***/
 
 /**
  * Draws rows according to buffer
@@ -196,24 +244,30 @@ void abFree(struct abuf *ab) {
  */
 void editorDrawRows(struct abuf *ab) {
     int y;
-    for (y = 0; y < E.screenrows; y++){
-        if (y == E.screenrows / 3) {
+    for (y = 0; y < E.screenrows; y++) {
+        if (y >= E.numrows) {
+            if (E.numrows == 0 && y == E.screenrows / 3) {
             char welcome[80];
             int welcomelen = snprintf(welcome, sizeof(welcome),
-              "Kilo editor -- version %s", KILO_VERSION);
+                "Kilo editor -- version %s", KILO_VERSION);
             if (welcomelen > E.screencols) welcomelen = E.screencols;
             int padding = (E.screencols - welcomelen) / 2;
             if (padding) {
-              abAppend(ab, "~", 1);
-              padding--;
+                abAppend(ab, "~", 1);
+                padding--;
             }
             while (padding--) abAppend(ab, " ", 1);
             abAppend(ab, welcome, welcomelen);
-          } else {
-            abAppend(ab, "~", 1);
-          }
-        abAppend(ab, "\x1b[K" , 3);
-        if (y<E.screenrows - 1) {
+            } else {
+                abAppend(ab, "~", 1);
+            }
+        } else {
+            int len = E.row[y].size;
+            if (len > E.screencols) len = E.screencols;
+            abAppend(ab, E.row[y].chars, len);
+        }
+        abAppend(ab, "\x1b[K", 3);
+        if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
         }
     }
@@ -240,7 +294,7 @@ void editorRefreshScreen() {
     abFree(&ab);
   }
 
-/*** input ***/
+/*** ------------------------- input ------------------------- ***/
 
 /**
  * @param key editorKey value of key pressed
@@ -308,17 +362,24 @@ void editorProcessKeypress() {
     }
 }
 
-/*** init ***/
+/*** ------------------------- init ------------------------- ***/
 
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.numrows = 0;
+    E.row = NULL;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main(){
+int main(int argc, char *argv[]){
     enableRawMode();
     initEditor();
+
+    if (argc >= 2) {
+        editorOpen(argv[1]);
+    }
 
     while (1){
         editorRefreshScreen();
