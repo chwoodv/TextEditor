d392b58673bfb720209d418df691c1cac3275103
Charles Wood <chwoodv@gmail.com>
2025-06-02 22:50:43 +0100

    Created status message first line

--- kilo.c
+++ kilo.c
@@ -14,6 +14,7 @@
 #include <sys/types.h>
 #include <string.h>
 #include <time.h>
+#include <stdarg.h>
 
 /*** ------------------------- defines ------------------------- ***/
 
@@ -53,7 +54,7 @@ struct editorConfig {
     int numrows;
     erow *row;
     char *filename;
-    char *statusmsg[80];
+    char statusmsg[80];
     time_t statusmsg_time;
     int screenrows;
     int screencols;
@@ -373,6 +374,17 @@ void editorDrawStatusBar(struct abuf *ab) {
         }
     }
     abAppend(ab, "\x1b[m", 3);
+    abAppend(ab, "\r\n", 2);
+}
+
+void editorDrawMessageBar(struct abuf *ab) {
+    abAppend(ab, "\x1b[K", 3);
+
+    int msglen = strlen(E.statusmsg);
+    if (msglen > E.screencols) msglen = E.screencols;
+    if (msglen && (time(NULL) - E.statusmsg_time < 5)) {
+        abAppend(ab, E.statusmsg, msglen);
+    }
 }
 
 /**
@@ -388,6 +400,7 @@ void editorRefreshScreen() {
     
     editorDrawRows(&ab);
     editorDrawStatusBar(&ab);
+    editorDrawMessageBar(&ab);
     
     char buf[32];
     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
@@ -397,7 +410,15 @@ void editorRefreshScreen() {
     
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
-  }
+}
+
+void editorSetStatusMessage(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+    va_end(ap);
+    E.statusmsg_time = time(NULL);
+}
 
 /*** ------------------------- input ------------------------- ***/
 
@@ -504,7 +525,7 @@ void initEditor() {
     E.statusmsg_time = 0;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
-    E.screenrows -= 1;
+    E.screenrows -= 2;
 }
 
 int main(int argc, char *argv[]){
@@ -514,6 +535,8 @@ int main(int argc, char *argv[]){
     if (argc >= 2) {
         editorOpen(argv[1]);
     }
+
+    editorSetStatusMessage("HELP: Ctrl-Q = quit");
 
     while (1){
         editorRefreshScreen();
